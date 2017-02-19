cfc2606c74276357981c4aa05f5029c987f5ae76
zelazko <zelazk.o@email.cz>
2017-02-19 11:50:14 +0100

    migration to postgresql + heroku test deploy

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 source 'https://rubygems.org'
 
-ruby '2.4.0'
+ruby '2.3.3'
 
 gem 'rails', '~> 5.0.0' # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'sqlite3' # Use sqlite3 as the database for Active Record
+gem 'pg'
 gem 'puma', '~> 3.0' # Use Puma as the app server
 # gem 'redis', '~> 3.0' # Use Redis adapter to run Action Cable in production
 # gem 'bcrypt', '~> 3.1.7' # Use ActiveModel has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161229193616) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                                  null: false
     t.string   "encrypted_password",                     null: false
@@ -26,8 +29,8 @@ ActiveRecord::Schema.define(version: 20161229193616) do
     t.boolean  "admin",                  default: false, null: false
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
